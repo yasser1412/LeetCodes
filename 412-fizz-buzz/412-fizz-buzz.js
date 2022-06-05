08b7e57bda211acd7695f9ce829aa8fdbd79b46f
Yasser Nasser <yassernasser1412@gmail.com>
2022-06-05 16:29:57 +0200

    Time: 103 ms (30.29%), Space: 44.6 MB (37.84%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let list = [];
+    for(let i=1; i<=n; i++){
+        if(i%3==0 && i%5==0){
+            list.push("FizzBuzz");
+        }else if(i%3==0){
+            list.push("Fizz");
+        }else if(i%5==0){
+            list.push("Buzz")
+        }else{
+            list.push(i.toString());
+        }
+    }
+    return list;
+};
